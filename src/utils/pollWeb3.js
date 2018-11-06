02322f7de2384672c66182d6e320680bd95363ed
makehoney <makehoney3@gmail.com>
2018-11-06 22:15:27 +0900

    remove useless part

--- src/utils/pollWeb3.js
+++ src/utils/pollWeb3.js
@@ -35,18 +35,3 @@ const pollWeb3 = state => {
 
 export default pollWeb3
 
-// const web3 = new Web3(window.web3.currentProvider)
-// let account = (await web3.eth.getAccounts())[0]
-// console.log(account)
-// let accountInterval = setInterval(async () => {
-//     let newAccount = (await web3.eth.getAccounts())[0]
-//     console.log(newAccount)
-//     if(newAccount !== account) {
-//         account = newAccount
-//         resolve(account)
-//     }
-// }, 100)
-//
-// accountInterval
-
-
