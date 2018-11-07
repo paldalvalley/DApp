88aadd357342556d583ccb23323b2c5dd3b350d6
makehoney <makehoney3@gmail.com>
2018-11-07 22:23:00 +0900

    refactor ipfs module

--- src/utils/ipfs/ipfs-service.js
+++ src/utils/ipfs/ipfs-service.js
@@ -1,38 +1,38 @@
-import IPFS from 'ipfs-api'
+import IPFS_API from 'ipfs-api'
 import { IPFS_CONFIG } from '../../config'
-const ipfs = IPFS(IPFS_CONFIG)
+const ipfs = IPFS_API(IPFS_CONFIG)
 
 /**
  * reason for using async function: to use try catch
  * */
 
-export const loadFromIPFS = async ipfsHash => {
-    try {
-        return await ipfs.files.get(ipfsHash)
-    } catch (err) {
-        throw new Error(err)
-    }
-}
-
-export const saveOneIntoIPFS = async json => {
-    try {
-        let jsonStr = JSON.stringify(json)
-        files.push({content: ipfs.types.Buffer.from(jsonStr)})
-        return await ipfs.files.add(files)
-    } catch (err) {
-        throw new Error(err)
-    }
-}
-
-export const saveManyIntoIPFS = async jsonArray => {
-    try {
-        let files = []
-        jsonArray.forEach(json => {
+export const IPFS = {
+    async load () {
+        try {
+            return await ipfs.files.get(ipfsHash)
+        } catch (err) {
+            throw new Error(err)
+        }
+    },
+    async saveOne () {
+        try {
             let jsonStr = JSON.stringify(json)
             files.push({content: ipfs.types.Buffer.from(jsonStr)})
-        })
-        return await ipfs.files.add(files)
-    } catch (err) {
-        throw new Error(err)
+            return await ipfs.files.add(files)
+        } catch (err) {
+            throw new Error(err)
+        }
+    },
+    async saveMany () {
+        try {
+            let files = []
+            jsonArray.forEach(json => {
+                let jsonStr = JSON.stringify(json)
+                files.push({content: ipfs.types.Buffer.from(jsonStr)})
+            })
+            return await ipfs.files.add(files)
+        } catch (err) {
+            throw new Error(err)
+        }
     }
 }
