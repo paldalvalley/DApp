f04f63ce2c9b1d3fb26226e4e55cb93f448bdfa9
makehoney <makehoney3@gmail.com>
2018-12-07 04:13:46 +0900

    Add contracts

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 var DietManager = artifacts.require("./DietManager.sol");
-
+var LoomyToken = artifacts.require("./LoomyToken.sol")
 module.exports = function(deployer) {
-  deployer.deploy(DietManager)
+  deployer.deploy(LoomyToken)
+    .then(() => {
+      return deployer.deploy(DietManager, LoomyToken.address)
+    })
+  // deployer.deploy(DietManager, LoomyToken.address)
+  //
+  // deployer.deploy(B).then(function() {
+  //   return deployer.deploy(A, B.address);
+  // });
 }
