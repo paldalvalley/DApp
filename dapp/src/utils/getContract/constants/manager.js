0ca7a2a6b376cf0d18e8a9933bc125122febe942
makehoney <makehoney3@gmail.com>
2018-12-07 04:09:44 +0900

    Change the way of importing ABI and contract address

--- dapp/src/utils/getContract/constants/manager.js
+++ dapp/src/utils/getContract/constants/manager.js
@@ -1,14 +1,11 @@
 import truffleJSON from '../../../../../truffle/build/contracts/DietManager'
 
 const networks = truffleJSON.networks
-// const keys = Object.keys(networks)
-// const lastIdxOfKeys = keys.length - 1
+const keys = Object.keys(networks)
+const lastIdxOfKeys = keys.length - 1
 
 const ABI = truffleJSON.abi
-const address = networks['4649'].address
-// const address = networks[keys[lastIdxOfKeys]].address
-// const address = '0x1a782cc3fb11a481bf6e0f35437af3a049c6ac62'
-// console.log(ABI)
-console.log(address)
+// const address = networks['4649'].address
+const address = networks[keys[lastIdxOfKeys]].address
 
 export { ABI, address }
