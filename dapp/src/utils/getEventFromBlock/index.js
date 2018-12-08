645f1f707884f0408ced7e78cd77db8f70ffb15f
makehoney <makehoney3@gmail.com>
2018-12-08 09:43:05 +0900

    Add getEventsFromBlock util

---
+++ dapp/src/utils/getEventFromBlock/index.js
@@ -0,0 +1,30 @@
+import { lib } from "../../modules/lib"
+import store from '../../store'
+
+export const getEventsFromBlock = async (eventName, filter) => {
+  let listingArray = []
+  const option = {
+    fromBlock: 0,
+    toBlock: 'latest'
+  }
+
+  if (filter) option.filter = filter
+
+  const events = await store.state.blockSync
+    .contractInstance()
+    .getPastEvents(eventName, option)
+
+  console.log('events is ')
+  console.log(events)
+
+  for (let i in events) {
+    const listingData = events[i].returnValues
+    const obj = Object.assign(
+      { listingID: listingData.listingID },
+      { ipfsHash: listingData.ipfsHash },
+      { party: listingData.party  })
+    listingArray.push(obj)
+    listingArray[i].data = await lib.ipfsService.loadObjFromFile(listingArray[i].ipfsHash)
+  }
+  return listingArray
+}
