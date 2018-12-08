5a2204fd1d851f44fb98c4fc2f160ba07a25fd83
makehoney <makehoney3@gmail.com>
2018-12-08 09:37:40 +0900

    Add BuyModal component

--- dapp/src/components/approved/Approved.vue
+++ dapp/src/components/approved/Approved.vue
@@ -3,6 +3,7 @@
         <search-bar/>
         <listings/>
         <post-modal/>
+        <buy-modal/>
     </div>
 </template>
 
@@ -10,12 +11,14 @@
     import SearchBar from './SearchBar'
     import Listings from './Listings'
     import PostModal from './PostModal'
+    import BuyModal from './BuyModal'
     export default {
         name: 'approved',
         components: {
             SearchBar,
             Listings,
-            PostModal
+            PostModal,
+            BuyModal
         }
     }
 
